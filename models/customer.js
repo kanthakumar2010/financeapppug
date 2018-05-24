d276fb75732d463d3b3d94d61e9fe8646c9e43ce
kanthakumar2010 <kanthakumar2010@gmail.com>
2018-05-24 18:33:47 +0530

    change addCustomer to Add Method Name, getCustomerById to findById

--- models/customer.js
+++ models/customer.js
@@ -24,14 +24,14 @@ const CustomerSchema = mongoose.Schema({
 const Customer = module.exports = mongoose.model('Customer', CustomerSchema)
 
 //Add new Customer
-module.exports.addCustomer = (newCustomer, callback) => {
+module.exports.add = (newCustomer, callback) => {
     newCustomer.save(callback)
 }
 
 //Get Customer Details By Querying IdNumber
-module.exports.getCustomerByidNumber = (_idNumber, callback) => {
+module.exports.findById = (_idPrimary, callback) => {
     const query = {
-        _id : _idNumber
+        _id : _idPrimary
     }
     //console.log(query)
     Customer.findOne(query, callback);
