45bee57cecde2cacdba8f1660be0f3fdd241f1da
Ragulan <ragulan312@gmail.com>
2018-05-29 23:25:51 +0530

    add loan to customer model

--- models/customer.js
+++ models/customer.js
@@ -18,7 +18,17 @@ const CustomerSchema = mongoose.Schema({
     address : {type: String},
     state : {type: String},
     city : {type: String},
-    pincode : {type: String}
+    pincode : {type: String},
+    loan : { 
+        number : {type: String},
+        type : {type : String},
+        principal : {type :String},
+        intrestRate : {type : String},
+        intrestAmount : {type : String},
+        emi : {type : String},
+        months : {type : String},
+        createdDate : { type: Date, default: Date.now } 
+    }
 })
 
 const Customer = module.exports = mongoose.model('Customer', CustomerSchema)
